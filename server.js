8b0249b3d2be3ff2daea41d7f5a1a2b4b47c94ce
patelnk4 <patelnk4@vcu.edu>
2022-06-24 14:29:08 -0500

    added port and mongo connections

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ const cors = require('cors')
 const MongoClient =require('mongodb').MongoClient
 
 require('dotenv').config()
+
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName='ntalk',                                          //change name here
+    collection
+
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('chat')
+    })
+
+
+    //PORT = 8000
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`Server is running on port`)
+}) 
