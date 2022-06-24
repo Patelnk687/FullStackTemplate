1c8528b702f5c8b06f94edafda00855ab5b48bb5
patelnk4 <patelnk4@vcu.edu>
2022-06-24 16:34:29 -0500

    added end point in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const app= express()
 const cors = require('cors')
+const { response } = require('express')
 const MongoClient =require('mongodb').MongoClient
 
 require('dotenv').config()
@@ -25,7 +26,14 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
+app.get('/', async(req,res)=>{
+try{response.render('index.ejs')
 
+} catch (error) {
+response.status(500).send({message: error.message})
+}
+
+})
 
     //PORT = 8000
 app.listen(process.env.PORT || PORT, () =>{
